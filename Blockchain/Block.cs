7bc35bf17adce9accce5babde1d00f5fc711d1fd
karastift <kiansinan@gmail.com>
2021-10-26 20:56:22 +0200

    Projektdateien hinzufügen.

---
+++ Blockchain/Block.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain
+{
+    class Block
+    {
+        public byte[] Hash { get; set; }
+        public byte[] PrevHash { get;set; }
+        public List<Transaction> Transactions { get; set; }
+        public string Time { get; set; }
+        public int Index { get; set; }
+        public int Nonce { get; set; }
+
+        public Block(byte[] prevHash, List<Transaction> transactions, int index, int nonce)
+        {
+            this.Time = DateTime.Now.ToString();
+            this.PrevHash = prevHash;
+            this.Transactions = transactions;
+            this.Index = index;
+            this.Nonce = nonce;
+            this.CalculateHash();
+        }
+        public void CalculateHash()
+        {
+            this.Hash = HelperFunctions.GetSHA256Hash(this.Stringify());
+        }
+
+        public string Stringify()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(this.PrevHash);
+            sb.Append(this.BuildTransactionsHash());
+            sb.Append(this.Time);
+            sb.Append(this.Index);
+            sb.Append(this.Nonce);
+
+            return sb.ToString();
+        }
+
+        private string BuildTransactionsHash()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this.Transactions.Count; i++)
+            {
+                sb.Append(this.Transactions[i].hash);
+            }
+
+            return sb.ToString();
+        }
+
+        public void Mine()
+        {
+            Console.WriteLine("Mining a block...");
+
+            Spinner spinner = new Spinner(Console.CursorLeft, Console.CursorTop, 100);
+
+            string solution = "00";
+
+            spinner.Start();
+            bool mined = false;
+
+            while (!mined)
+            {
+                if (HelperFunctions.ConvertToHexString(this.Hash).StartsWith(solution))
+                {
+                    spinner.Stop();
+                    Console.WriteLine("Finished mined a block.");
+                    mined = true;
+                }
+                else
+                {
+                    this.Nonce++;
+                    this.CalculateHash();
+                }
+            }
+        }
+    }
+}
---
+++ Blockchain/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+
+namespace Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Blockchain blockchain = new Blockchain();
+            Wallet wallet = new Wallet();
+
+/*            for (int i = 0; i < 12; i++)
+            {
+                wallet.SendMoney(2, "you", blockchain);
+            }
+*/
+            wallet.SendMoney(2, "you", blockchain);
+            blockchain.MinePendingTransactions(wallet);
+
+            Console.ReadKey();
+        }
+    }
+}
